dd77b013bb0e014ef53a2dcb94e9e2e4f71f351d
Artur <looppinooppi@gmail.com>
2024-04-04 14:07:36 +0300

    feat: input

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput style={styles.input} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 300,
+    paddingVertical: 4,
+    paddingHorizontal: 10,
+    borderColor: '#000000',
+    borderWidth: 1,
+    borderStyle: 'solid',
+  }
 });
