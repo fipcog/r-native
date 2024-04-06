f8779f3763afdee33e716ff9d78c54daacf4caa9
Artur <looppinooppi@gmail.com>
2024-04-06 12:13:41 +0300

    feat: main props type

--- appNavigatorTypes.ts
+++ appNavigatorTypes.ts
@@ -1,5 +1,9 @@
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+
 export type RootStackParamList = {
   Main: undefined
   Topics: undefined
   Questions: undefined
 }
+
+export type MainProps = NativeStackScreenProps<RootStackParamList, 'Main'>
